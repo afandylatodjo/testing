54d3cb0edba55d3cd5d762094780447dba111749
afandylatodjo <ahmadafandy660@gmail.com>
2025-05-17 13:05:48 +0800

    fix: perbaikan kode  issue #13

--- src/main.py
+++ src/main.py
@@ -2,11 +2,11 @@ def main():
     print("Hello Worl")
 
     list = [1,2,3,4,(1,2), "A", 5]
-    repeat(vals)
+    repeat(list)
 
   
 def repeat(vals)->int:
     for i in vals:
         print(i)
 
-  
+main()  
